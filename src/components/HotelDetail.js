29c8604fcf0f772f2e485848d8571370befeb13d
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-02-26 15:16:08 -0800

    create hotelList functionality with connection to firestore

---
+++ src/components/HotelDetail.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+function HotelDetail(props) {
+  const { hotel } = props;
+
+  return(
+    <>
+    <h1>Hotel Details</h1>
+    <h3>{hotel.name}</h3>
+    <h3>{hotel.location}</h3>
+    <p><em>{hotel.reviews}</em></p>
+    <br></br>
+    {/* <button onClick = { props.onClickingEdit }>Update Hotel Info/Review</button> */}
+    <button onClick ={() => onClickingDelete(hotel.id)}>Delete Hotel/Review?</button>
+    </>
+  );
+}
+
+HotelDetail.propTypes = {
+  hotel: PropTypes.object,
+  onClickingDelete: PropTypes.func
+  // onClickingEdit: PropTypes.func
+}
+
+export default HotelDetail;
---
+++ src/components/HotelList.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Hotel from './Hotel';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
+
+function HotelList(props) {
+
+  useFirestoreConnect([
+    { collection: 'hotels' }
+  ]);
+
+  const hotels = useSelector(state => state.firestore.ordered.hotels);
+
+  if(isLoaded(hotels)) {
+    return(
+      <>
+      <hr/>
+      {hotels.map((hotel) => {
+        return <Hotel
+          whenHotelClicked = { props.onHotelSelection }
+          name = { hotel.name }
+          location = { hotel.location }
+          review = { hotel.review }
+          id = { hotel.id }
+          key = { hotel.id }/>
+      })}
+      <hr></hr>
+      </>
+    );
+  } else {
+    return (
+      <>
+      <h3>Loading....</h3>
+      </>
+    )
+  }
+}
+
+HotelList.propTypes = {
+  onHotelSelection: PropTypes.func
+};
+
+export default HotelList;
