3e0f19bd3594ea5905c918d3fa0f2fec88d38140
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-02-26 14:53:48 -0800

    create Hotel.js with proptypes, will add more later once I decide on what specific info to display

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -1,1 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+function Hotel (props) {
+  return(
+    <>
+    <div className = 'card' id = 'hotelEntry'>
+      <div className = 'row'>
+        <div onClick = {() => props.whenHotelClicked(props.id)}>
+          <h3>{props.name}</h3>
+          <h4>{props.location}</h4>
+          <p>{props.reviews}</p>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+Hotel.propTypes = {
+  name: PropTypes.string,
+  location: PropTypes.string,
+  reviews: PropTypes.string,
+  id: PropTypes.string,
+  whenHotelClicked: PropTypes.func
+};
+
+export default Hotel;
