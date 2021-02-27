82d2c4f2a71fe1cabe889927015d5a63f18b8b61
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-02-26 16:45:54 -0800

    create action folder with needed files

--- src/components/HotelControl.js
+++ src/components/HotelControl.js
@@ -1,3 +1,24 @@
 import React from 'react';
 import NewHotelForm from './NewHotelForm';
-import HotelList from './'
+import HotelList from './HotelList';
+import HotelDetail from './HotelDetail';
+import PropTypes from "prop-types";
+import { connect } from 'react-redux';
+import { withFirestore, isLoaded } from 'react-redux-firebase';
+
+class HotleControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedMemory: null,
+      
+    };
+  }
+
+  handleAddingNewHotelToList = () => {
+    const { dispatch } = this.props;
+    const action = a.toggleForm();
+    dispatch(action);
+  }
+}
