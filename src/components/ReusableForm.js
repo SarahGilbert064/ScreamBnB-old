38353b59625f697e23a2240ba448f885b476103e
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-03-05 14:44:59 -0800

    change textarea of form and have it connect to firebase

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -4,25 +4,28 @@ import PropTypes from 'prop-types';
 function ReusableForm(props) {
   return (
     <>
-      <form onSubmit={props.formSubmissionHandler}>
-        <input 
-          type='text'
-          name='name'
-          placeholder="Hotel Name"/>
-        <br></br>
-        <input
-          type='text'
-          name='location'
-          placeholder='Hotel Location'/>
-        <br></br>  
-        <input
-          type='text'
-          name='review'
-          placeholder='Give it a Reveiw'/>
-        <br></br> 
-        <button type='submit'>{props.buttonText}</button>
-        <br></br>
-      </form>
+      <center>
+        <form onSubmit={props.formSubmissionHandler}>
+          <input 
+            type='text'
+            name='name'
+            placeholder="Hotel Name"/>
+          <br></br>
+          <input
+            type='text'
+            name='location'
+            placeholder='Hotel Location'/>
+          <br></br> 
+          <textarea name='review' cols="40" rows="5"> 
+          <input
+            type='text'
+            placeholder='Give it a Reveiw'/>
+          </textarea> 
+          <br></br> 
+          <button type='submit'>{props.buttonText}</button>
+          <br></br>
+        </form>
+      </center>
     </>
   );
 }
