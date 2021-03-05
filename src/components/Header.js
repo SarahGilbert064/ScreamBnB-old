6f9d4611a8dc245981ca6469c05f6c95ef97402f
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-03-05 15:37:25 -0800

    finally get my logo image to show up as header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-import React from "react";
-// import logo from './../img/logo.png';
+import React, { Component } from "react"
+import logo from './../img/logo.png';
 
 
 function Header() {
@@ -16,9 +16,13 @@ function Header() {
   }
   return (
     <React.Fragment>
-      <img src={require('./../img/logo.png')} width='500' height='600'>
-
-      </img>
+      <div className='row'>
+        <div className='logo'>
+          <center>
+            <img src={logo} width="250" height="200" margin="50px"/>
+          </center>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
