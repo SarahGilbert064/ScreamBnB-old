19d8214f2b058d7d76e1056a34badae1766f9c67
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-03-05 09:16:25 -0800

    add click handles to hotelControl

--- src/components/HotelControl.js
+++ src/components/HotelControl.js
@@ -5,6 +5,7 @@ import HotelDetail from './HotelDetail';
 import PropTypes from "prop-types";
 import { connect } from 'react-redux';
 import { withFirestore, isLoaded } from 'react-redux-firebase';
+import Hotel from './Hotel';
 
 class HotleControl extends React.Component {
 
@@ -12,7 +13,7 @@ class HotleControl extends React.Component {
     super(props);
     this.state = {
       selectedMemory: null,
-      
+      editing: false
     };
   }
 
@@ -21,4 +22,38 @@ class HotleControl extends React.Component {
     const action = a.toggleForm();
     dispatch(action);
   }
+
+  handleChangingSelectedHotel = (id) => {
+    this.props.withFirestore
+    .get({collection: 'hotels', doc: id})
+    .then((memory)  => {
+      const firestoreHotel = {
+        name: hotel.get('name'),
+        location: hotel.get('location'),
+        review: hotel.get('review'),
+        id: hotel.id
+      }
+      this.setState({selectedHotel: firestoreHotel});
+    });
+  }
+
+  handleDeletingHotel = (id) => {
+    this.props.firestore.delete({collection: 'hotels', doc: id});
+    this.setState({selectedHotel: null});
+  }
+
+  handleEditClick = () => {
+    this.setState({
+      editing: true 
+    });
+  }
+
+  handleEditingHotelInList = () => {
+    this.setState({
+      editing: false,
+      selectedHotel: null 
+    });
+  }
+
+  
 }
